b95304552e9fe2e8f65c7ce32adcda1b5cf19c2c
Arash <arashm81@gmail.com>
2021-02-15 09:13:19 -0800

    Do not use! moving to version 3

---
+++ version-2/df.py
@@ -0,0 +1,260 @@
+import os
+import altair as alt
+import pandas as pd
+import numpy as np
+from collections import OrderedDict
+from glob import glob
+
+class AppCtx:
+    pass
+
+def create_data_frame_from_file(folder_name,meta_data_filename, parse_meta_data_fun, parse_content_fun, keywords, df_col_names, appCtx):
+    
+    fnames=[]
+    for f in os.listdir(folder_name):
+        if f[-4:] == '.log':
+            fnames.append(f)
+    
+    logFiles = sorted(fnames)
+    
+    #change directory to where log files are
+    os.chdir(os.path.join(os.getcwd(),folder_name))
+    
+    #parse user meta-data using user-defined function
+    nu_vals, h_lght, odh, odp = parse_meta_linE(meta_data_filename)
+
+    h = []
+    for k in odh:
+        for el in odh[k]:
+            h.append(digitize(el))
+
+    psz=[]
+    for k in odh:
+        psz.append(len(odh[k]))
+
+    ps=[]
+    for k in odp:
+        for el in odp[k]:
+            ps.append(el)
+
+    p=[]
+    for i in range(len(ps)):
+        for j in range(psz[i]):
+            p.append(ps[i])
+
+    nusz=[] 
+    counter = 0
+    for item in h_lght:
+        k=0
+        for i in range(item):
+            k=k+psz[counter]
+            counter=counter+1
+        nusz.append(k)
+    
+    nu = []
+    for i in range(len(nu_vals)):
+        for j in range(nusz[i]):
+            nu.append(nu_vals[i])
+
+    #for Matlab plotting use (same data as in run.info)
+    ffm= open("for-Matlab.txt","w+")
+    for i in range(len(nusz)):
+        ffm.write("%i " %nusz[i])
+    ffm.write("\n")
+##    for i in range(len(vals)):
+##        ffm.write("%i " %vals[i])
+    ffm.close()
+    
+    dof = []        
+    ksp = [] 
+    solveTime = []
+    snes_dof_per_sec = []
+    L2err = []
+    num_proc = []
+    total_time = []
+    for filename in logFiles:
+        f = open(filename, 'r')
+        lines = f.readlines()
+        for line in lines:
+            if keywords[0] in line:
+                ll = line.strip().split()
+                dof.append(int(ll[-1])*appCtx.dof)
+            elif keywords[1] in line:
+                ll = line.strip().split()
+                ksp.append(int(ll[-1]))
+            elif keywords[2] in line:
+                ll = line.strip().split()
+                solveTime.append(float(ll[-3]))
+            elif keywords[3] in line:
+                ll = line.strip().split()
+                snes_dof_per_sec.append(float(ll[-3]))
+            elif keywords[4] in line:
+                ll = line.strip().split()
+                L2err.append(float("{:.7e}".format(float(ll[-1]))))
+            elif keywords[5] in line:
+                ll = line.strip().split()
+                total_time.append(float("{:.4e}".format(float(ll[2]))))
+            elif keywords[6] in line:
+                ll = line.strip().split()
+                num_proc.append(int(ll[7]))
+        f.close()
+    
+        
+    dict_vals = [nu, p, h, dof, ksp, solveTime,snes_dof_per_sec,L2err,total_time,num_proc]
+    ordered_dict = OrderedDict()
+    for i in range(len(df_col_names)):
+        ordered_dict[df_col_names[i]] = dict_vals[i]
+
+    dataFrame = pd.DataFrame(ordered_dict)
+
+    #change durectory back to where you were
+    os.chdir("..")
+
+    return dataFrame
+
+
+
+def parse_meta_linE(meta_data_filename):
+
+    #run.info format:
+    #
+    #vaiables used in run script
+    #polynomials
+    #nu's
+    #p1 p2 ... pn   <--n values
+    #h's for p1
+    #h's for p2
+    #...
+    #h's for pn      <--nth row after
+    #p1 p2 ... pn   <--n values
+    #...
+    #...
+
+    md_lst = []
+    f = open(meta_data_filename,'r')
+    for line in f.readlines():
+        md_lst.append(line.split())
+    f.close()
+    
+    var_names = md_lst[0]
+    deg_vals = []
+    nu_vals = []
+    for i in range(len(md_lst[1])):
+        deg_vals.append(digitize(md_lst[1][i]))
+
+    for i in range(len(md_lst[2])):
+        nu_vals.append(digitize(md_lst[2][i]))
+
+   
+    #md_lst[3] is 1st occurance of p's used
+    counter = 3
+    p_idx =[]
+    h_lght=[]
+    for i in range(len(md_lst)):
+        if i < counter:
+            continue
+        else:
+            p_idx.append(counter)
+            counter = counter+len(md_lst[i])+1
+            h_lght.append(len(md_lst[i]))
+
+    odp = OrderedDict()
+    k=0
+    for idx in p_idx:
+        tmpv=[]
+        for v in md_lst[idx]:
+            tmpv.append(digitize(v))            
+        odp[k] = tmpv
+        k=k+1
+
+    print(p_idx)
+    partial_var_names=[]
+    for idx in p_idx:
+        for thing in md_lst[idx]:
+            print(thing)
+            partial_var_names.append('hp'+thing+'nu')
+
+    nu_str = md_lst[2]
+    print(partial_var_names)
+
+    #running out of time:
+    #hard code a little. Fix later!
+    #Need much better meta-data really
+    vnm=partial_var_names
+    vnm[0] = vnm[0] + nu_str[0]
+    vnm[1] = vnm[1] + nu_str[0]
+    vnm[2] = vnm[2] + nu_str[0]
+    vnm[3] = vnm[3] + nu_str[0]
+    vnm[4] = vnm[4] + nu_str[1]
+    vnm[5] = vnm[5] + nu_str[1]
+    vnm[6] = vnm[6] + nu_str[1]
+    vnm[7] = vnm[7] + nu_str[2]
+    vnm[8] = vnm[8] + nu_str[2]
+    vnm[9] = vnm[9] + nu_str[2]
+    vnm[10] = vnm[10] + nu_str[3]
+    vnm[11] = vnm[11] + nu_str[3]
+    vnm[12] = vnm[12] + nu_str[3]
+
+    print(len(vnm))
+
+    data_idx = list(range(3, len(md_lst)))
+    for val in p_idx:
+        data_idx.remove(val)
+
+    print(len((data_idx)))
+    odh = OrderedDict()
+    for i in range(len(vnm)):
+        odh[vnm[i]] = md_lst[data_idx[i]]
+    
+    return nu_vals, h_lght, odh , odp 
+    
+        
+def parse_linE():
+    pass
+
+#helper function to convert str numbers to int or float
+def digitize(item):
+    if '.' in item:
+        item.replace('.', '', 1).isdigit()
+        return float(item)
+    elif item.isdigit():
+        return int(item)
+    else:
+        return
+                
+
+if __name__ == "__main__":
+
+    folder_name = 'log_files'
+    meta_data_filename = 'run.info'
+    parse_meta_data_fun = parse_meta_linE #function pointer
+    keywords = ['Global nodes','Total KSP Iterations', 'SNES Solve Time', 'DoFs/Sec in SNES', 'L2 Error', 'Time (sec):', './elasticity', ]
+    df_col_names= ['nu', 'p', 'h', 'DoF', '#CG', 'Solve Time(s)', 'MDoFs/Sec', 'L2 Error', 'Total Time(sec)','np']
+    parse_content_fun = parse_linE #function pointer
+    #pass in do (make a struct/class later)
+    appCtx=AppCtx()
+    appCtx.dof=3
+
+    df = create_data_frame_from_file(folder_name,meta_data_filename, parse_meta_data_fun, parse_content_fun, keywords, df_col_names, appCtx)
+
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', -1)
+
+    #print(df)
+    print(df['Total Time(sec)'].sum())
+    
+
+    df.to_csv (r'data.csv', index = False, header=True)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ version-2/mms_run.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+hp1nu3=(25 26 75 76 100 125)
+hp2nu3=(10 11 18 19 32 33)
+hp3nu3=(4 5 6 7 10 11)
+hp4nu3=(2 3 4 5 6 8)
+
+nus=(0.3 0.49 0.49999 0.499999)
+deg=(1 2 3 4)
+
+#echo "nu=0.3 and p=1 refinement is running ..."
+#for h in ${hp1nu3[@]};do
+#   mpirun -n 64 ./elasticity -degree 1 -E 1 -nu 0.3 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"001_nu_0.3_deg_1.log"
+
+#echo "nu=0.3 and p=2 refinement is running ..."
+#for h in ${hp2nu3[@]};do
+#   mpirun -n 64 ./elasticity -degree 2 -E 1 -nu 0.3 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"002_nu_0.3_deg_2.log"
+
+#echo "nu=0.3 and p=3 refinement is running ..."
+#for h in ${hp3nu3[@]};do
+#   mpirun -n 64 ./elasticity -degree 3 -E 1 -nu 0.3 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"003_nu_0.3_deg_3.log"
+
+#echo "nu=0.3 and p=4 refinement is running ..."
+#for h in ${hp4nu3[@]};do
+#   mpirun -n 64 ./elasticity -degree 4 -E 1 -nu 0.3 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"004_nu_0.3_deg_4.log"
+
+#echo "All runs for nu=0.3 finished!"
+
+echo "p nu h" &>log_files/run.info
+for p in ${deg[@]};do
+   printf "$p " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for nu in ${nus[@]};do
+   printf "$nu " >>log_files/run.info
+done
+
+printf "\n1 2 3 4\n" >>log_files/run.info
+for h in ${hp1nu3[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp2nu3[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp3nu3[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp4nu3[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+
+
+hp2nu49=(11 12 22 23 40 41)
+hp3nu49=(5 6 8 9 12 13)
+hp4nu49=(4 7 8 9 12 13)
+
+#echo "nu=0.49 and p=2 refinement is running ..."
+#for h in ${hp2nu49[@]};do
+#   mpirun -n 64 ./elasticity -degree 2 -E 1 -nu 0.49 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"005_nu_0.49_deg_2.log"
+
+#echo "nu=0.49 and p=3 refinement is running ..."
+#for h in ${hp3nu49[@]};do
+#   mpirun -n 64 ./elasticity -degree 3 -E 1 -nu 0.49 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"006_nu_0.49_deg_3.log"
+
+#echo "nu=0.49 and p=4 refinement is running ..."
+#for h in ${hp4nu49[@]};do
+#   mpirun -n 64 ./elasticity -degree 4 -E 1 -nu 0.49 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"007_nu_0.49_deg_4.log"
+
+#echo "All runs for nu=0.49 finished!"
+
+#printf "\n2 3 4\n" >>log_files/run.info
+#for h in ${hp2nu49[@]}; do
+#   printf "$h " >>log_files/run.info
+#done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp3nu49[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp4nu49[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+
+hp2nu49999=(12 13 37 38)
+hp3nu49999=(7 8 16 17)
+hp4nu49999=(5 6 10 11)
+
+#echo "nu=0.49999 and p=2 refinement is running ..."
+#for h in ${hp2nu49999[@]};do
+#   mpirun -n 64 ./elasticity -degree 2 -E 1 -nu 0.49999 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"008_nu_0.49999_deg_2.log"
+
+#echo "nu=0.49999 and p=3 refinement is running ..."
+#for h in ${hp3nu49999[@]};do
+#   mpirun -n 64 ./elasticity -degree 3 -E 1 -nu 0.49999 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"009_nu_0.49999_deg_3.log"
+
+#echo "nu=0.49999 and p=4 refinement is running ..."
+#for h in ${hp4nu49999[@]};do
+#   mpirun -n 64 ./elasticity -degree 4 -E 1 -nu 0.49999 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"010_nu_0.49999_deg_4.log"
+
+#echo "All runs for nu=0.49999 finished!"
+
+
+printf "\n2 3 4\n" >>log_files/run.info
+for h in ${hp2nu49999[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp3nu49999[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp4nu49999[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+
+hp2nu499999=(12 13 38 39)
+hp3nu499999=(4 5 7 8)
+hp4nu499999=(5 6 10 11)
+
+#echo "nu=0.499999 and p=2 refinement is running ..."
+#for h in ${hp2nu499999[@]};do
+#   mpirun -n 64 ./elasticity -degree 2 -E 1 -nu 0.499999 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"011_nu_0.499999_deg_2.log"
+
+#echo "nu=0.499999 and p=3 refinement is running ..."
+#for h in ${hp3nu499999[@]};do
+#   mpirun -n 64 ./elasticity -degree 3 -E 1 -nu 0.499999 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"012_nu_0.499999_deg_3.log"
+
+#echo "nu=0.499999 and p=4 refinement is running ..."
+#for h in ${hp4nu499999[@]};do
+#   mpirun -n 64 ./elasticity -degree 4 -E 1 -nu 0.499999 -dm_plex_box_faces $h,$h,$h -forcing mms -log_view
+#done | grep 'L2 Error\|SNES Solve Time\|DoFs/Sec in SNES\|Total KSP Iterations\|Global nodes\|Time (sec)\|noether with' > log_files/"013_nu_0.499999_deg_4.log"
+
+#echo "All runs for nu=0.499999 finished!"
+
+printf "\n2 3 4\n" >>log_files/run.info
+for h in ${hp2nu499999[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp3nu499999[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+for h in ${hp4nu499999[@]}; do
+   printf "$h " >>log_files/run.info
+done
+
+printf "\n" >>log_files/run.info
+
+
+
