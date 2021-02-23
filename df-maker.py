691a2fbb6fb2338724e62a47fe5272d9b9bc1abc
Arash <arashm81@gmail.com>
2021-02-23 15:43:21 -0800

    complete with plots

--- df-maker.py
+++ df-maker.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from collections import OrderedDict
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.lines import Line2D
 
 class AppCtx:
     pass
@@ -124,7 +126,8 @@ def create_df_linE_noether(filenames_data , files_data):
     dff["np"] = dff["np"].astype(int)
     return dff
 
-def plot_linE_noether(df):
+#3D plots are NOT appropriate for this study
+def plot_linE_noether_3D(df):
     #filters:
     #p 
     p1 = df['p']==1
@@ -132,6 +135,8 @@ def plot_linE_noether(df):
     p3 = df['p']==3
     p4 = df['p']==4
     # np (num processors)
+    np1 = df['np']== 1
+    np4 = df['np']== 4
     np16 = df['np']==16
     np32 = df['np']==32
     np64 = df['np']==64
@@ -140,101 +145,250 @@ def plot_linE_noether(df):
     nu49 = df['nu']==0.49
     nu49999 = df['nu']==0.49999
     nu499999 = df['nu']>(0.49999) #For some reason nu499999 = df['nu']==0.499999 does not work!
-##    err = df.where((np64 & nu3 & (df['L2 Error'] > 1e5) & (df['L2 Error'] < 1e6))).dropna()
-##    print(err)
-##    test_err = df['L2 Error']> 1e-5
-##    print(test_err)
-                   
-                   
-                  
-                   
-    # 
-    nus = np.unique(df['nu'])
+    #
+    #err_4 means error with the order of 10^(-4) 
+    err_4 = ((df['L2 Error'] < 1e-3) & (df['L2 Error'] > 1e-4))
+    #err_5 means error with the order of 10^(-5)
+    err_5 = ((df['L2 Error'] < 1e-4) & (df['L2 Error'] > 1e-5))
+    #err_5 means error with the order of 10^(-6)
+    err_6 = ((df['L2 Error'] < 1e-5) & (df['L2 Error'] > 1e-6))
 
-    pComp_filter = [p1,p2,p3,p4]
-    #np_filter = [np16, np32, np64]
+    num_nps = np.unique(df['np'])
+    nps = [np1, np4, np16, np32, np64]
+
+    plt_marker = ['p', '*' , 'o', '^' , 'x']
+    plt_linestyle = ['--g','--r', '--b','--k', '--m' ]
+    plt_color = ['g','r', 'b','k', 'm' ]
+    fig = plt.figure()
     
+    # FOR nu = 0.3 & error :10^(-4)       
+    ax = fig.add_subplot(2, 2, 1, projection='3d')
+    for i in range(len(nps)):
+        h = df.where((nps[i] & nu3 & err_4))['h'].dropna()
+        p = df.where((nps[i] & nu3 & err_4))['p'].dropna()
+        t = df.where((nps[i] & nu3 & err_4))['Total Time(s)'].dropna()
+        #surf = ax.plot(p, h, t, plt_linestyle[i], label="np = {}".format(num_nps[i]), marker=plt_marker[i])
+        scat = ax.scatter(p,h,t, c=plt_color[i], label="np = {}".format(num_nps[i]), marker=plt_marker[i])
+        ax.set_xlabel('poly order')
+        ax.set_ylabel('h')        
+        ax.set_zlabel('Time(s)')
+        ax.set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+        #ax.set_title(r'$\nu = 0.3$')
+        ax.legend(loc="upper left", title='L$^2$ Error: 10$^{-4}$', shadow=True)
 
-    P_i_nu_3 = []
-    err_i_nu_3 = []
-    tot_time_i_nu_3 = []
-    for i in range(len(pComp_filter)):
-        h_p_i_nu3_np64 = df.where((np64 & nu3 & pComp_filter[i]))['h'].dropna()
-        P_i_nu_3.append(df.where((np64 & nu3 & h_p_i_nu3_np64))['p'].dropna())
-        err_i_nu_3.append(df.where((np64 & nu3 & pComp_filter[i]))['L2 Error'].dropna())
-        tot_time_i_nu_3.append(df.where((np64 & nu3 & pComp_filter[i]))['Total Time(s)'].dropna())
-  
-    
-    plt_marker = ['p', '*' , 'o', '^']
-    plt_linestyle = ['--g','--r', '--b','--k' ]
-    fig, ax = plt.subplots(2,2)
-    for i in range(len(P_i_nu_3)):
-        ax[0][0].semilogy(P_i_nu_3[i], err_i_nu_3[i], plt_linestyle[i], marker=plt_marker[i], label="p{}".format(i+1))
-        ax[0][0].set_ylabel('L2 Error')
-        ax[0][0].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
-        ax[0][0].set_title(r'$\nu = 0.3$')
-        ax[0][0].legend(loc="upper right", shadow=True)
+    # FOR nu = 0.3 & error :10^(-5)       
+    ax = fig.add_subplot(2, 2, 2, projection='3d')
+    for i in range(len(nps)):
+        h = df.where((nps[i] & nu3 & err_5))['h'].dropna()
+        p = df.where((nps[i] & nu3 & err_5))['p'].dropna()
+        t = df.where((nps[i] & nu3 & err_5))['Total Time(s)'].dropna()
+        #surf = ax.plot(p, h, t, plt_linestyle[i], label="np = {}".format(num_nps[i]), marker=plt_marker[i])
+        scat = ax.scatter(p,h,t, c=plt_color[i], label="np = {}".format(num_nps[i]), marker=plt_marker[i])
+        ax.set_xlabel('poly order')
+        ax.set_ylabel('h')        
+        ax.set_zlabel('Time(s)')
+        ax.set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+        #ax.set_title(r'$\nu = 0.3$')
+        ax.legend(loc="upper left", title='L$^2$ Error: 10$^{-5}$', shadow=True)
 
-    pIncomp_filter = [p2,p3,p4]
+    # FOR nu = 0.3 & error :10^(-6)       
+    ax = fig.add_subplot(2, 2, 3, projection='3d')
+    for i in range(len(nps)):
+        h = df.where((nps[i] & nu3 & err_6))['h'].dropna()
+        p = df.where((nps[i] & nu3 & err_6))['p'].dropna()
+        t = df.where((nps[i] & nu3 & err_6))['Total Time(s)'].dropna()
+        #surf = ax.plot(p, h, t, plt_linestyle[i], label="np = {}".format(num_nps[i]), marker=plt_marker[i])
+        scat = ax.scatter(p,h,t, c=plt_color[i], label="np = {}".format(num_nps[i]), marker=plt_marker[i])
+        ax.set_xlabel('poly order')
+        ax.set_ylabel('h')        
+        ax.set_zlabel('Time(s)')
+        ax.set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+        #ax.set_title(r'$\nu = 0.3$')
+        ax.legend(loc="upper left", title='L$^2$ Error: 10$^{-6}$', shadow=True)
 
-    P_i_nu_49 = []
-    err_i_nu_49 = []
-    tot_time_i_nu_49 = []
-    for i in range(len(pIncomp_filter)):
-        h_p_i_nu49_np64 = df.where((np64 & nu49 & pIncomp_filter[i]))['h'].dropna()
-        P_i_nu_49.append(df.where((np64 & nu49 & h_p_i_nu49_np64))['p'].dropna())
-        err_i_nu_49.append(df.where((np64 & nu49 & pIncomp_filter[i]))['L2 Error'].dropna())
-        tot_time_i_nu_49.append(df.where((np64 & nu49 & pIncomp_filter[i]))['Total Time(s)'].dropna())
-  
-    for i in range(len(P_i_nu_49)):
-        ax[0][1].semilogy(P_i_nu_49[i], err_i_nu_49[i], plt_linestyle[i+1], marker=plt_marker[i+1], label="p{}".format(i+2))
-        ax[0][1].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
-        ax[0][1].set_title(r'$\nu = 0.49$')
-        ax[0][1].legend(loc="upper right", shadow=True)
-
-    P_i_nu_49999 = []
-    err_i_nu_49999 = []
-    tot_time_i_nu_49999 = []
-    for i in range(len(pIncomp_filter)):
-        h_p_i_nu49999_np64 = df.where((np64 & nu49999 & pIncomp_filter[i]))['h'].dropna()
-        P_i_nu_49999.append(df.where((np64 & nu49999 & h_p_i_nu49999_np64))['p'].dropna())
-        err_i_nu_49999.append(df.where((np64 & nu49999 & pIncomp_filter[i]))['L2 Error'].dropna())
-        tot_time_i_nu_49999.append(df.where((np64 & nu49999 & pIncomp_filter[i]))['Total Time(s)'].dropna())
-  
-    for i in range(len(P_i_nu_49999)):
-        ax[1][0].semilogy(P_i_nu_49999[i], err_i_nu_49999[i], plt_linestyle[i+1], marker=plt_marker[i+1], label="p{}".format(i+2))
-        ax[1][0].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
-        ax[1][0].set_ylabel('L2 Error')
-        ax[1][0].set_xlabel('poly order')
-        ax[1][0].set_title(r'$\nu = 0.49999$')
-        ax[1][0].legend(loc="upper right", shadow=True)
-
-    P_i_nu_499999 = []
-    err_i_nu_499999 = []
-    tot_time_i_nu_499999 = []
-    for i in range(len(pIncomp_filter)):
-        h_p_i_nu499999_np64 = df.where((np64 & nu499999 & pIncomp_filter[i]))['h'].dropna()
-        P_i_nu_499999.append(df.where((np64 & nu499999 & h_p_i_nu499999_np64))['p'].dropna())
-        err_i_nu_499999.append(df.where((np64 & nu499999 & pIncomp_filter[i]))['L2 Error'].dropna())
-        tot_time_i_nu_499999.append(df.where((np64 & nu499999 & pIncomp_filter[i]))['Total Time(s)'].dropna())
-  
-    for i in range(len(P_i_nu_499999)):
-        ax[1][1].semilogy(P_i_nu_499999[i], err_i_nu_499999[i], plt_linestyle[i+1], marker=plt_marker[i+1], label="p{}".format(i+2))
-        ax[1][1].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
-        ax[1][1].set_xlabel('poly order')
-        ax[1][1].set_title(r'$\nu = 0.499999$')
-        ax[1][1].legend(loc="upper right", shadow=True)
-
-    #fig.suptitle('Polynomial order vs. $L^2$ Error on 64 processors')
-    plt.savefig('pVSerr.eps', format='eps')
-    manager = plt.get_current_fig_manager()
-    manager.resize(*manager.window.maxsize())
+    fig.suptitle(r'$\nu = 0.3$')
     plt.show()
+##    #Best time for 10^(-4)
+##    df4 = df.where((nu3 & err_4)).dropna()
+##    print(df.where(df4['Total Time(s)'] == df4['Total Time(s)'].min()).dropna())
+##
+##   # df = df.                              .sort_values(["np", "nu", "p", "h", "run"], ascending = (True, True,True,True,True))
+##    dftt = df.where((nu3 & err_4)).dropna().sort_values(['Total Time(s)'], ascending = (True))
+##    print(dftt)
+
+
+
+def plot_linE_noether_2D(df):
+    #filters:
+    #p 
+    p1 = df['p']==1
+    p2 = df['p']==2
+    p3 = df['p']==3
+    p4 = df['p']==4
+    # np (num processors)
+    np1 = df['np']== 1
+    np4 = df['np']== 4
+    np16 = df['np']==16
+    np32 = df['np']==32
+    np64 = df['np']==64
+    # nu
+    nu3 = df['nu']==0.3
+    nu49 = df['nu']==0.49
+    nu49999 = df['nu']==0.49999
+    nu499999 = df['nu']>(0.49999) #For some reason nu499999 = df['nu']==0.499999 does not work!
+    #
+    #err_4 means error with the order of 10^(-4) 
+    err_4 = ((df['L2 Error'] < 1e-3) & (df['L2 Error'] > 1e-4))
+    #err_5 means error with the order of 10^(-5)
+    err_5 = ((df['L2 Error'] < 1e-4) & (df['L2 Error'] > 1e-5))
+    #err_5 means error with the order of 10^(-6)
+    err_6 = ((df['L2 Error'] < 1e-5) & (df['L2 Error'] > 1e-6))
+
+    nps = [np1, np4, np16, np32, np64]
+
+    proc=[1,4,16,32,64]
+    pComp = [p1,p2,p3,p4]
+    pIncomp = [p2,p3,p4]
+
+    legend_elements = [Line2D([0], [0], marker='p', label='p1',markersize=10),
+                       Line2D([0], [0], marker='*', label='p2',markersize=10),
+                       Line2D([0], [0], marker='o', label='p3',markersize=10),
+                       Line2D([0], [0], marker='^', label='p4',markersize=10),
+                       Line2D([0], [0], marker='s', color='k', label='1 cpu'),
+                       Line2D([0], [0], marker='s', color='b', label='4 cpu'),
+                       Line2D([0], [0], marker='s', color='r', label='16 cpu'),
+                       Line2D([0], [0], marker='s', color='g', label='32 cpu'),
+                       Line2D([0], [0], marker='s', color='orange', label='64 cpu'),] 
+                # p1   p2    p3   p4
+    plt_marker = ['p', '*' , 'o', '^']    
+    plt_color = ['k', 'b', 'r', 'g', 'orange']
+    fig, ax = plt.subplots(2,2)
+    for i in range(len(pComp)):
+        for j in range(len(nps)):
+            h = df.where((nu3 & pComp[i] & nps[j] & err_4))['h'].dropna()
+            t = df.where((nu3 & pComp[i] & nps[j] & err_4))['Total Time(s)'].dropna()          
+            ax[0][0].scatter(h, t, c=plt_color[j], marker=plt_marker[i]) #, label=pl_label) #"p{}".format(i+1))
+            ax[0][0].set_ylabel('Time(s)')
+            ax[0][0].set_xlabel('h')
+            ax[0][0].set_title(r'$\nu = 0.3$')
+            ax[0][0].legend(ncol=10, handles=legend_elements, loc="upper center", bbox_to_anchor=(1.1, 1.25), shadow=True)
+            #ax[0][0].legend( ncol=4, handleheight=0.5, labelspacing=0.05, loc="upper center", bbox_to_anchor=(1.1, 1.25), shadow=True)
+
+    for i in range(len(pIncomp)):
+        for j in range(len(nps)):
+            h = df.where((nu49 & pIncomp[i] & nps[j] & err_4))['h'].dropna()
+            print(h)
+            t = df.where((nu49 & pIncomp[i] & nps[j] & err_4))['Total Time(s)'].dropna()          
+            ax[0][1].scatter(h, t, c=plt_color[j], marker=plt_marker[i+1]) 
+            ax[0][1].set_ylabel('Time(s)')
+            ax[0][1].set_xlabel('h')
+            ax[0][1].set_title(r'$\nu = 0.49$')
+
+    for i in range(len(pIncomp)):
+        for j in range(len(nps)):
+            h = df.where((nu49999 & pIncomp[i] & nps[j] & err_4))['h'].dropna()
+            t = df.where((nu49999 & pIncomp[i] & nps[j] & err_4))['Total Time(s)'].dropna()          
+            ax[1][0].scatter(h, t, c=plt_color[j], marker=plt_marker[i+1]) 
+            ax[1][0].set_ylabel('Time(s)')
+            ax[1][0].set_xlabel('h')
+            ax[1][0].set_title(r'$\nu = 0.49999$')
+            
+    for i in range(len(pIncomp)):
+        for j in range(len(nps)):
+            h = df.where((nu499999 & pIncomp[i] & nps[j] & err_4))['h'].dropna()
+            t = df.where((nu499999 & pIncomp[i] & nps[j] & err_4))['Total Time(s)'].dropna()          
+            ax[1][1].scatter(h, t, c=plt_color[j], marker=plt_marker[i+1]) 
+            ax[1][1].set_ylabel('Time(s)')
+            ax[1][1].set_xlabel('h')
+            ax[1][1].set_title(r'$\nu = 0.499999$')
+    plt.show()
+        
+    
+    
+
+##    P_i_nu_3 = []
+##    err_i_nu_3 = []
+##    tot_time_i_nu_3 = []
+##    for i in range(len(pComp_filter)):
+##        h_p_i_nu3_np64 = df.where((np64 & nu3 & pComp_filter[i]))['h'].dropna()
+##        P_i_nu_3.append(df.where((np64 & nu3 & h_p_i_nu3_np64))['p'].dropna())
+##        err_i_nu_3.append(df.where((np64 & nu3 & pComp_filter[i]))['L2 Error'].dropna())
+##        tot_time_i_nu_3.append(df.where((np64 & nu3 & pComp_filter[i]))['Total Time(s)'].dropna())
+##  
+##                # p1   p2    p3   p4
+##    plt_marker = ['s', '*' , 'o', '^'] 
+##    plt_linestyle = ['g','r', 'b','k' ]
+##    
+##    plt_color = ['orange','b','r','g','b','k' ]
+##    fig, ax = plt.subplots(2,2)
+##    for i in range(len(pComp_filter)):
+##        h = df.where((nu3, pComp_filter[i], nps[i] & err_4))['h'].dropna()
+##        ax[0][0].scatter(P_i_nu_3[i], err_i_nu_3[i], c=plt_color[i], marker=plt_marker[i], label="p{}".format(i+1))
+##        ax[0][0].set_ylabel('Time(s)')
+##        ax[0][0].set_xlabel('h')
+##        ax[0][0].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+##        ax[0][0].set_title(r'$\nu = 0.3$')
+##        ax[0][0].legend(loc="upper right", shadow=True)
+##
+##    pIncomp_filter = [p2,p3,p4]
+##
+##    P_i_nu_49 = []
+##    err_i_nu_49 = []
+##    tot_time_i_nu_49 = []
+##    for i in range(len(pIncomp_filter)):
+##        h_p_i_nu49_np64 = df.where((np64 & nu49 & pIncomp_filter[i]))['h'].dropna()
+##        P_i_nu_49.append(df.where((np64 & nu49 & h_p_i_nu49_np64))['p'].dropna())
+##        err_i_nu_49.append(df.where((np64 & nu49 & pIncomp_filter[i]))['L2 Error'].dropna())
+##        tot_time_i_nu_49.append(df.where((np64 & nu49 & pIncomp_filter[i]))['Total Time(s)'].dropna())
+##  
+##    for i in range(len(P_i_nu_49)):
+##        ax[0][1].semilogy(P_i_nu_49[i], err_i_nu_49[i], plt_linestyle[i+1], marker=plt_marker[i+1], label="p{}".format(i+2))
+##        ax[0][1].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+##        ax[0][1].set_title(r'$\nu = 0.49$')
+##        ax[0][1].legend(loc="upper right", shadow=True)
+##
+##    P_i_nu_49999 = []
+##    err_i_nu_49999 = []
+##    tot_time_i_nu_49999 = []
+##    for i in range(len(pIncomp_filter)):
+##        h_p_i_nu49999_np64 = df.where((np64 & nu49999 & pIncomp_filter[i]))['h'].dropna()
+##        P_i_nu_49999.append(df.where((np64 & nu49999 & h_p_i_nu49999_np64))['p'].dropna())
+##        err_i_nu_49999.append(df.where((np64 & nu49999 & pIncomp_filter[i]))['L2 Error'].dropna())
+##        tot_time_i_nu_49999.append(df.where((np64 & nu49999 & pIncomp_filter[i]))['Total Time(s)'].dropna())
+##  
+##    for i in range(len(P_i_nu_49999)):
+##        ax[1][0].semilogy(P_i_nu_49999[i], err_i_nu_49999[i], plt_linestyle[i+1], marker=plt_marker[i+1], label="p{}".format(i+2))
+##        ax[1][0].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+##        ax[1][0].set_ylabel('L2 Error')
+##        ax[1][0].set_xlabel('poly order')
+##        ax[1][0].set_title(r'$\nu = 0.49999$')
+##        ax[1][0].legend(loc="upper right", shadow=True)
+##
+##    P_i_nu_499999 = []
+##    err_i_nu_499999 = []
+##    tot_time_i_nu_499999 = []
+##    for i in range(len(pIncomp_filter)):
+##        h_p_i_nu499999_np64 = df.where((np64 & nu499999 & pIncomp_filter[i]))['h'].dropna()
+##        P_i_nu_499999.append(df.where((np64 & nu499999 & h_p_i_nu499999_np64))['p'].dropna())
+##        err_i_nu_499999.append(df.where((np64 & nu499999 & pIncomp_filter[i]))['L2 Error'].dropna())
+##        tot_time_i_nu_499999.append(df.where((np64 & nu499999 & pIncomp_filter[i]))['Total Time(s)'].dropna())
+##  
+##    for i in range(len(P_i_nu_499999)):
+##        ax[1][1].semilogy(P_i_nu_499999[i], err_i_nu_499999[i], plt_linestyle[i+1], marker=plt_marker[i+1], label="p{}".format(i+2))
+##        ax[1][1].set_xticks([0, 1, 2, 3, 4, 5], ['1', '2','3','4','5'])
+##        ax[1][1].set_xlabel('poly order')
+##        ax[1][1].set_title(r'$\nu = 0.499999$')
+##        ax[1][1].legend(loc="upper right", shadow=True)
+
+##    #fig.suptitle('Polynomial order vs. $L^2$ Error on 64 processors')
+##    plt.savefig('pVSerr.eps', format='eps')
+##    manager = plt.get_current_fig_manager()
+##    manager.resize(*manager.window.maxsize())
+##    plt.show()
 ##    figure = plt.gcf()  # get current figure
 ##    figure.set_size_inches(32, 18) # set figure's size manually to your full screen (32x18)
 ##    plt.savefig('pVSerr.png', bbox_inches='tight')
 ##    plt.savefig('pVSerr.eps', format='eps')
-    
 
 def digitize(item):
     if '.' in item:
@@ -284,9 +438,12 @@ if __name__ == "__main__":
    
     #create a dataframe
     df = create_df_linE_noether(filenames_data , files_data)
-  
-    plot_linE_noether(df)
-    print(df)
+
+    #Does not look good
+    #plot_linE_noether_3D(df)
+
+    plot_linE_noether_2D(df)
+    #print(df)
 
     
 
